5e8d2912a99e521aed2e6b860d565fbf942ecd30
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-15 23:44:42 +0000

    finished with footer component
    - I have completed work on the footer component.
    - The component is responsive and works well on mobile and tablet devices.
    - I have added some new devDependencies, such as the daisyui library. Please remember to run "npm install" after I merge this branch.
    - I also made some changes to the home component because the div and CSS were too generic.
    - Can someone please review the PR? Thanks

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import '../home/home.css';
+import Footer from "../footer/Footer";
 
 const Home = () => {
   const [getData, setGetData] = useState([]);
@@ -21,16 +22,21 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-container text-sky-600">
-      <h1> Welcome to the Reality</h1>
-      <h1>Team Unity</h1>
-      {getData.map((data) => (
-        <ul key={data.id}>
-          <li className="home-list">
-            id: {data.id}, Module: {data.module_name}
-          </li>
-        </ul>
-      ))}
+    <div>
+      <div className="home-container lg:text-[50px] sx:text-[50px]">
+        <h1> Welcome to the Reality</h1>
+        <h1>Team Unity</h1>
+        {getData.map((data) => (
+          <ul key={data.id}>
+            <li className="home-list">
+              id: {data.id}, Module: {data.module_name}
+            </li>
+          </ul>
+        ))}
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
