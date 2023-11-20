eed8da5aaca0a466d096dcdbafab5e10aa72bfb2
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-20 17:32:15 +0000

    add logo and Title into top of side panel

--- client/src/components/navBar/SidePanel.jsx
+++ client/src/components/navBar/SidePanel.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BsArrowLeftShort } from "react-icons/bs";
-// import { AiFillBell } from "react-icons/ai";
-// import { BsSearch } from "react-icons/bs";
+import { AiFillBell } from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
 
 
 const SidePanel = () => {
@@ -18,6 +18,23 @@ const SidePanel = () => {
           ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
+
+        <div className="inline-flex">
+          <AiFillBell
+            className={`bg-amber-300 p-2 text-7xl rounded cursor-pointer block float-left mr-4 duration-500 ${
+              open && "rotate-[360deg]"
+            }`}
+          />
+          <h1
+            className={`text-white origin-left font-semibold pt-6 text-[15px] duration-300 ${
+              !open && "scale-0"
+            }`}
+          >
+            CYF Spaced Repetition
+          </h1>
+        </div>
+
+
       </div>
     </div>
   );
