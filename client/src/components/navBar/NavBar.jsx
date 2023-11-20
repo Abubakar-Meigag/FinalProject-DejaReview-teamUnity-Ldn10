1637cf2ef8924da8faa5e5d2b387f507a66bf564
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-20 17:43:33 +0000

    add Dashboard into the panel and link it with home page

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { MdDashboardCustomize } from "react-icons/md";
+import { LuClipboardEdit } from "react-icons/lu";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 
-const NavBar = () => {
+
+const NavBar = ({ open }) => {
   return (
-    <div>NavBar</div>
-  )
+    <div className="flex mt-16">
+      <ul>
+
+        <div>
+          <li className="inline-flex">
+            <Link to="/">
+              <MdDashboardCustomize
+                className={`bg-amber-300 p-2 rounded cursor-pointer block float-left mr-4 duration-500 ${
+                  open ? "text-7xl" : "text-5xl"
+                }
+                    `}
+              />
+            </Link>
+            <Link
+              to="/"
+              className={`text-white origin-left font-semibold pt-3 text-[30px] cursor-pointer ${
+                !open && "scale-0"
+              }`}
+            >
+              Dashboard
+            </Link>
+          </li>
+        </div>
+
+        
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar
