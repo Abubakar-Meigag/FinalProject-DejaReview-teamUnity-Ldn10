367f31e9ae57865d018d9441a9444b87216d05ea
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-11-20 14:45:49 +0000

    create Route & Routes in app.js  and import BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
-
+import Management from "./components/management/Management";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
-    <div>
-      <Home />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/management" element={<Management />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
