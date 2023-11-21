04957e9368156a1cda1c1b1cc4ccff609c315c4c
kristina_dudnyk <kristina.dudnyk20@gmail.com>
2023-11-21 13:21:15 +0000

    created a table for a dashboard

--- server/database/data.sql
+++ server/database/data.sql
@@ -1,26 +1,31 @@
 --  All our data schema can be displayed here. For instance, we can split all tables, and each of us can create a table.
-
-
 -- Add create modules table 
 CREATE TABLE modules (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
 	description VARCHAR,
-  created_at timestamp DEFAULT now() 
-)
-
--- Add users table
+	created_at timestamp DEFAULT now()
+) -- Add users table
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255) NOT NULL, 
-	email VARCHAR(255) NOT NULL UNIQUE 
+	last_name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE
 )
-
-INSERT INTO users (first_name, last_name, email)
+INSERT INTO
+	users (first_name, last_name, email)
 VALUES
-  ('Elena', 'Barker', 'elena@gmail.com'),
-  ('Abubakar', 'Meigag', 'beko@gmail.com'),
-  ('Kristina', 'Dudnyk', 'kristina@gmail.com'),
-  ('Joseph', 'Mwanza' 'joseph@gmail.com');
+	('Elena', 'Barker', 'elena@gmail.com'),
+	('Abubakar', 'Meigag', 'beko@gmail.com'),
+	('Kristina', 'Dudnyk', 'kristina@gmail.com'),
+	('Joseph', 'Mwanza' 'joseph@gmail.com');
 
+-- Dashboard table
+CREATE TABLE dashboard (
+	id SERIAL PRIMARY KEY,
+	topic_id INT,
+	user_id INT,
+	task_status ENUM('Not Started', 'In Progress'),
+	due_date timestamp,
+	reviews_remaining INT DEFAULT 5
+)
