1842beddfe4c8124abd239cc1b3700a75fd26a70
Joemwa <109852314+Joemwa@users.noreply.github.com>
2023-11-22 22:37:30 +0000

    Added getTopics to index.js

--- server/index.js
+++ server/index.js
@@ -30,3 +30,26 @@ app.get("/test", getDataTest);
 const getDataUsers = require("./endPoints-file/usersEndPoint");
 
 app.get("/users", getDataUsers);
+
+
+
+
+const pool = require("../database/db");
+
+const getTopics = async (req, res) => {
+  const query = "SELECT * FROM topics";
+  try {
+    const data = await pool.query(query);
+    res.status(200).json(data.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      result: "failure",
+      message: "No data found",
+    });
+  }
+};
+
+module.exports = {
+  getTopics,
+};
