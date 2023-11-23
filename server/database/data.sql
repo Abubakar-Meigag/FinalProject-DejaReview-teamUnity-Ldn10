5598c2f7d635c3a4717920c083cc81e90ea2a7f2
Kristina <kristina.dudnyk20@gmail.com>
2023-11-23 13:37:25 +0000

    On the server side, in folder data.sql was created a dashboard table with all required columns https://drawsql.app/teams/aas-4/diagrams/copy-of-deja-review

--- server/database/data.sql
+++ server/database/data.sql
@@ -1,17 +1,20 @@
 --  All our data schema can be displayed here. For instance, we can split all tables, and each of us can create a table.
--- Add create modules table 
+--  Add create modules table 
 CREATE TABLE modules (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
 	description VARCHAR,
 	created_at timestamp DEFAULT now()
-) -- Add users table
+);
+
+-- Add users table
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE
-)
+);
+
 INSERT INTO
 	users (first_name, last_name, email)
 VALUES
@@ -21,11 +24,11 @@ VALUES
 	('Joseph', 'Mwanza' 'joseph@gmail.com');
 
 -- Dashboard table
-CREATE TABLE dashboard (
+CREATE TABLE learning_topics_tracker (
 	id SERIAL PRIMARY KEY,
 	topic_id INT,
 	user_id INT,
-	task_status ENUM('Not Started', 'In Progress'),
+	task_status INT DEFAULT 0,
 	due_date timestamp,
-	reviews_remaining INT DEFAULT 5
-)
+	reviews_remaining INT DEFAULT 4
+);
