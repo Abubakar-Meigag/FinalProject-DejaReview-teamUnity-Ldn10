d6653a6942754a577584182ae5bebf8094728c4d
Kristina <kristina.dudnyk20@gmail.com>
2023-11-24 22:53:34 +0000

    Conect backend to frontend on AllModulesPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,36 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/home/Home";
 import AllModulesPage from "./components/All_Modules_Page/AllModulesPage";
 import Management from "./components/management/Management";
 import SidePanel from "./components/navBar/SidePanel";
 
 function App() {
+  const [refreshModalData, setrefreshModalData] = useState(false);
+
   return (
     <div className="flex">
       <BrowserRouter>
         <SidePanel />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AllModulesPage" element={<AllModulesPage />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                refreshModalData={refreshModalData}
+                setrefreshModalData={setrefreshModalData}
+              />
+            }
+          />
+          <Route
+            path="/AllModulesPage"
+            element={
+              <AllModulesPage
+                refreshModalData={refreshModalData}
+                setrefreshModalData={setrefreshModalData}
+              />
+            }
+          />
           <Route path="/management" element={<Management />} />
         </Routes>
       </BrowserRouter>
--- client/src/components/All_Modules_Page/AllModulesPage.jsx
+++ client/src/components/All_Modules_Page/AllModulesPage.jsx
@@ -1,52 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./AllModulesPage.css";
 import CardForTopic from "./Card_For_Topic/Card_for_topic";
-const fakedata = [
-  {
-    module: "HTML||CSS",
-    topics: [
-      {
-        nameOfTopic: "Tags",
-        details:
-          "If you're a Dropbox Pro or Dropbox for Business user, you can control who accesses your links and for how long by setting passwords and expirations for them. Alternatively, you can use view-only permissions for shared folders. Links are ideal for replacing email attachments, whereas view-only permissions for shared folders are useful for keeping collaborators continuously up to date without giving them the ability to change files..",
-      },
-      {
-        nameOfTopic: "Styling",
-        details: "Array of ayyj ooen ooog hynslki oenvttb ioo, ppp.",
-      },
-    ],
-  },
-  {
-    module: "JavaScript 1",
-    topics: [
-      {
-        nameOfTopic: "Array",
-        details: "Array of ayyj ooen ooog hynslki oenvttb ioo, ppp.",
-      },
-      {
-        nameOfTopic: "Object",
-        details:
-          "If you're a Dropbox Pro or Dropbox for Business user, you can control who accesses your links and for how long by setting passwords and expirations for them. Alternatively, you can use view-only permissions for shared folders. Links are ideal for replacing email attachments, whereas view-only permissions for shared folders are useful for keeping collaborators continuously up to date without giving them the ability to change files..",
-      },
-    ],
-  },
-  {
-    module: "React",
-    topics: [
-      {
-        nameOfTopic: "Components",
-        details:
-          "If you're a Dropbox Pro or Dropbox for Business user, you can control who accesses your links and for how long by setting passwords and expirations for them. Alternatively, you can use view-only permissions for shared folders. Links are ideal for replacing email attachments, whereas view-only permissions for shared folders are useful for keeping collaborators continuously up to date without giving them the ability to change files..",
-      },
-      {
-        nameOfTopic: "Imports",
-        details:
-          "If you're a Dropbox Pro or Dropbox for Business user, you can control who accesses your links and for how long by setting passwords and expirations for them. Alternatively, you can use view-only permissions for shared folders. Links are ideal for replacing email attachments, whereas view-only permissions for shared folders are useful for keeping collaborators continuously up to date without giving them the ability to change files..",
-      },
-    ],
-  },
-];
-export default function AllModulesPage() {
+
+export default function AllModulesPage({ refreshmodalData }) {
+  const [modalData, setModalData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_DB_URL}/allModulesPage`
+      );
+      console.log("response", response);
+
+      const data = await response.json();
+      console.log("data", data);
+
+      console.log("fetchData in modalData", data);
+      setModalData(data);
+    } catch (error) {
+      console.log("The ERROR occured in fetchData in DisplaymodalData:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [refreshmodalData]);
+
   const [openListIndex, setOpenListIndex] = useState(null);
 
   const handleListHeaderClick = (index) => {
@@ -54,11 +33,11 @@ export default function AllModulesPage() {
   };
 
   const [topicStates, setTopicStates] = useState(
-    fakedata.flatMap((element) => element.topics.map(() => false))
+    modalData.flatMap((element) => element.topics.map(() => false))
   );
 
   const toggleTopic = (moduleIndex, topicIndex) => {
-    const flatIndex = moduleIndex * fakedata[0].topics.length + topicIndex;
+    const flatIndex = moduleIndex * modalData[0].topics.length + topicIndex;
     const newTopicStates = [...topicStates];
     newTopicStates[flatIndex] = !newTopicStates[flatIndex];
     setTopicStates(newTopicStates);
@@ -68,34 +47,35 @@ export default function AllModulesPage() {
     <div className="modules-container">
       <h1 className="all-modules-header">All Modules</h1>
       <ul className="modules-list">
-        {fakedata.map((element, index) => (
+        {modalData.map((element, index) => (
           <li
             className={`list-item ${index === openListIndex ? "open" : ""}`}
             key={index}
           >
             <div
-              className="module-name"
+              className="module-info"
               onClick={() => handleListHeaderClick(index)}
             >
-              {element.module}
+              <h2>{element.module_name}</h2>
+              <p>{element.module_description}</p>
             </div>
             <div className="topics-container">
               <ul className="topics-list">
-                {element.topics.map((topic, subIndex) => (
+                {element.topics.map((element, subIndex) => (
                   <li
                     key={subIndex}
                     onClick={() => toggleTopic(index, subIndex)}
                     className="topics-container-inner"
                   >
-                    <h2>{topic.nameOfTopic}</h2>
+                    <h2>{element.topic_name}</h2>
                     <CardForTopic
                       showTopic={
                         topicStates[
-                          index * fakedata[0].topics.length + subIndex
+                          index * modalData[0].topics.length + subIndex
                         ]
                       }
                       toggleTopic={() => toggleTopic(index, subIndex)}
-                      topic={topic}
+                      topic={element}
                     />
                   </li>
                 ))}
