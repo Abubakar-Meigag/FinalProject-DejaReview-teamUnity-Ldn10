532b3a5e5f0b55096b2020ba3daa5ba08e1e9fb1
Joemwa <109852314+Joemwa@users.noreply.github.com>
2023-11-25 20:07:23 +0000

    Created input fields
    Created input fields for enter topic, description and reference link

--- client/src/components/dashboard/ModuleDropdown.jsx
+++ client/src/components/dashboard/ModuleDropdown.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './ModuleDropdown.css'
+
 
 const ModuleDropdown = () => {
   const [modules, setModules] = useState([]);
-  const [selectTopic, setTopics] = useState([]);
+  const [topicName, setTopicName] = useState('');
+  const [description, setDescription] = useState('');
+  const [referenceLink, setReferenceLink] = useState('');
 
   useEffect(() => {
-   
-    
     const fetchData = async () => {
       try {
         const response = await axios.get('https://deja-review-backend.onrender.com/modules');
@@ -16,49 +18,36 @@ const ModuleDropdown = () => {
         console.error('Error fetching modules:', error);
       }
     };
-
     fetchData();
-   
-  
-}, []);
+  }, []);
 
+ 
 
-
-
-useEffect(() => {
-   
-    
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://deja-review-backend.onrender.com/topics');
-        setTopics(response.data);
-      } catch (error) {
-        console.error('Error fetching modules:', error);
-      }
-    };
-
-    fetchData();
-   
-  
-}, [])
-
-  
   return (
-    <div>
-      
-        <select>
-          {modules.map((module) => (
-            <option key={module.id}>{module.name}</option>
-          ))}
-        </select>
+    <div className="ModuleDropdownContainer">
+      <h2>Create New Learning Topic</h2>
+      <select>
+        {modules.map((module) => (
+          <option key={module.id}>{module.name}</option>
+        ))}
+      </select>
 
-        <select>
-          {selectTopic.map((topics) => (
-            <option key={topics.id}>{topics.topic_name}</option>
-          ))}
-        </select>
-      
-      
+      <div>
+        <label>{'Topic Name:'}</label>
+        <input type="text" placeholder={'Enter Topic Name'} value={topicName} onChange={(e) => setTopicName(e.target.value)} />
+      </div>
+
+      <div>
+        <label>{'Description:'}</label>
+        <input type="text" placeholder={'Enter Description'} value={description} onChange={(e) => setDescription(e.target.value)} />
+      </div>
+
+      <div>
+        <label>{'Reference Link:'}</label>
+        <input type="text" placeholder={'Enter Reference Link'} value={referenceLink} onChange={(e) => setReferenceLink(e.target.value)} />
+      </div>
+
+      <button className='SendButton'>Send</button>
     </div>
   );
 };
