20bc0bfb11eafd2f3bd6873910b54fb04c4dc835
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-12-02 23:50:24 +0000

    create endpoint file to store new learning topic submitted by user
    this endpoint will store data with checking if user send to topics table by adding true as column

---
+++ server/endPoints-file/CreateNewTopic.js
@@ -0,0 +1,37 @@
+const pool = require("../database/db");
+
+const CreateNewTopic = async (req, res) => {
+  try {
+    const {
+        module_id,
+        topic_name,
+        description,
+        reference_link,
+        is_user_generated,
+    } = req.body;
+
+    if (!module_id || !topic_name || !description || !reference_link || !is_user_generated) {
+        return res.status(400).json({ error: "Invalid data" });
+    }
+
+    const insertQuery =
+      "INSERT INTO topics (module_id, topic_name, description, reference_link, test_link) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+
+    const result = await pool.query(insertQuery, [
+        module_id,
+        topic_name,
+        description,
+        reference_link,
+        is_user_generated,
+    ]);
+
+    res.status(201).json(result.rows[0]);
+    console.log(result.rows[0], 'data inserted successfully')
+    
+  } catch (error) {
+    console.log(error.detail);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = CreateNewTopic;
