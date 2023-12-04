b8999d1d6c2a8625a59d7b5c5cf6fdd53dc03647
Kristina <kristina.dudnyk20@gmail.com>
2023-12-04 22:04:33 +0000

    fixing spelling

--- client/src/components/dashboard/UpComingTopic.jsx
+++ client/src/components/dashboard/UpComingTopic.jsx
@@ -4,13 +4,12 @@ const UpComingTopic = ({ userTopics }) => {
   const firstThreeTopics = userTopics.modules.slice(0, 3);
   return (
     <div className="up-coming-topic-container">
-      <h2>UP COMMING TOPICS</h2>
+      <h2>UP COMING TOPICS</h2>
 
       {firstThreeTopics.map((topic) => (
         <div className="up-coming-topic-item" key={topic.entry_id}>
           <h3>{topic.topic_name}</h3>
           <h2>{new Date(topic.due_date).toDateString()}</h2>
-          <button> </button>
         </div>
       ))}
     </div>
